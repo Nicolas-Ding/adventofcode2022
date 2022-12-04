20fa98ec841902628b2c87dc3565b8ef118a0bb5
Nicolas Ding <6966170+Nicolas-Ding@users.noreply.github.com>
2022-12-04 17:58:23 +0100

    Day04 + refactoring of common code

---
+++ AdventOfCodeUtils/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCodeUtils
+open System.IO
+
+module AdventOfCode =
+    type ENV = T | P
+
+    let inputFile env = 
+        match env with
+        | T -> "test.txt"
+        | P -> "input.txt"
+
+    let lines env = File.ReadLines (inputFile env)
+
+    let run part = 
+        printfn "Test data : %A" (part (lines T))
+        printfn "Test data : %A" (part (lines P))
---
+++ Day04-FS/Program.fs
@@ -0,0 +1,25 @@
+﻿
+open System.Text.RegularExpressions
+open AdventOfCodeUtils.AdventOfCode
+
+let (|ParseData|_|) input =
+    let m = Regex.Match(input, @"(\d+)-(\d+),(\d+)-(\d+)")
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ] |> List.map int)
+    else None
+
+let compute filter line = 
+    match line with 
+    | ParseData [a; b; x; y] when filter a b x y || filter x y a b -> 1
+    | _ -> 0
+
+let isIncluded a b x y = (a >= x && b <= y)
+
+let hasOverlap a b x y = (a >= x && a <= y) || (b >= x && b <= y)
+
+let part isValid lines = 
+    lines
+    |> Seq.sumBy (compute isValid)
+
+run (part isIncluded)
+run (part hasOverlap)
+
