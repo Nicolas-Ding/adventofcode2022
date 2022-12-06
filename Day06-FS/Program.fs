b6d48c93aad713069a11c4c84f38ec9a5c5d4baa
Nicolas Ding <6966170+Nicolas-Ding@users.noreply.github.com>
2022-12-07 00:04:15 +0100

    Day06

---
+++ Day06-FS/Program.fs
@@ -0,0 +1,13 @@
+﻿open AdventOfCodeUtils.AdventOfCode
+
+let solve n = 
+    Seq.head
+    >> Seq.windowed n 
+    >> Seq.findIndex (Seq.distinct >> Seq.length >> (=) n)
+    >> (+) n
+
+let part1 = solve 4
+let part2 = solve 14
+
+run part1
+run part2
